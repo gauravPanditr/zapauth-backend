048ae47685aeef9f91b9ed5e0f35595afaed6f8d
gauravPanditr <gauravpandit7856@gmail.com>
2025-05-16 20:28:12 +0530

    db connected

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+import connectDB from './config/db';
+import dotenv from 'dotenv';
 
-app.use(express.json());
+dotenv.config(); // Load .env
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from TypeScript + Express!');
-});
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
 });
