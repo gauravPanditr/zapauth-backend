69b8763432de9ce9662f9837f90b839c663d5c74
gauravPanditr <gauravpandit7856@gmail.com>
2025-05-18 20:12:12 +0530

    making auth-middlewares code clean

--- src/middlewares/admin-auth.ts
+++ src/middlewares/admin-auth.ts
@@ -1,29 +1,45 @@
-import { Request, Response, NextFunction } from "express";
+import { NextFunction, Response } from "express";
+import jwt from "jsonwebtoken";
 import { AdminModel } from "../model/admin.model";
+import { IRequest } from "../types/api.types";
 
-
-
-export const adminAuth = async (
-  req: Request,
+export const authenticateAdmin = async (
+  req: IRequest,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    const token = req.headers["authorization"]?.replace("Bearer ", "");
-    if (!token) {
-      return res.status(401).json({ message: "Unauthorized" });
+    const accessToken =
+      req.headers.authorization?.replace("Bearer ", "") ||
+      req.cookies["admin-access-token"];
+
+    if (!accessToken) {
+      return res.status(401).json({ message: "Access token not provided" });
     }
-    const admin = await AdminModel.findOne({
-      accessToken: token,
-    }).select("-password");
-    if (!admin) {
-      return res.status(401).json({ message: "Unauthorized" });
+
+    const decodedToken = jwt.decode(accessToken) as { adminId: string } | null;
+
+    if (!decodedToken?.adminId) {
+      return res.status(401).json({ message: "Invalid access token" });
     }
-     
+
+    const adminFromDB = await AdminModel.findById(decodedToken.adminId).select(
+      "-password -refreshToken"
+    );
+
+    if (!adminFromDB) {
+      return res.status(404).json({ message: "Admin not found" });
+    }
+
+    if (adminFromDB.accessTokenExpiry < new Date()) {
+      return res.status(401).json({ message: "Access token expired" });
+    }
+
+    req.admin = { id: adminFromDB._id };
+
     next();
   } catch (error) {
-   
+    console.error("Error in authenticateAdmin:", error);
     return res.status(500).json({ message: "Internal Server Error" });
   }
-  
-}
+};
