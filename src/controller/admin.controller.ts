d6f69385c55b9d396e6edc848942836da1965c2e
gauravPanditr <gauravpandit7856@gmail.com>
2025-05-18 20:22:02 +0530

    making admin controller

--- src/controller/admin.controller.ts
+++ src/controller/admin.controller.ts
@@ -1,5 +1,32 @@
+import { Request, Response } from "express";
 
-
-
-
-
+export const createAdmin = {
+  async getAdmin(req:Request, res:Response) {
+    try {
+      const admin = req.body;
+      if (!admin) {
+        return res.status(404).json({ message: "Admin not found" });
+      }
+      res.status(200).json(admin);
+    }
+    catch (error) {
+      console.error("Error in getAdmin:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  },
+}
+export const getAdmin = {
+  async getAdmin(req:Request, res:Response) {
+    try {
+      const admin = req.body;
+      if (!admin) {
+        return res.status(404).json({ message: "Admin not found" });
+      }
+      res.status(200).json(admin);
+    }
+    catch (error) {
+      console.error("Error in getAdmin:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  },
+}
