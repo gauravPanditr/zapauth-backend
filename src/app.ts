609ff02cf5833cf99ce9b5c38e90bfefd06abffa
gauravPanditr <gauravpandit7856@gmail.com>
2025-05-19 20:06:56 +0530

    making app.ts file for router

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import { adminRouter } from "./routes/admin.routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use("/api", adminRouter);
+
+export default app;
