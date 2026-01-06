aa7475f161440d5eb13a400ff603d12eb605bcca
gauravPanditr <gauravpandit7856@gmail.com>
2026-01-06 19:33:57 +0530

    create admin singup and login

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import { connectMongo } from "./db/mongo";
 import { CONFIG } from "./config";
-
+import dotenv from "dotenv";
+import adminRoutes from "./routes/admin.routes";
 const app = express();
-app.use(express.json());
+
 
 async function startServer() {
   await connectMongo(CONFIG.mongoUri);
+dotenv.config();
+app.use(express.json());
+app.use("/api/admin",adminRoutes);
+
 
   app.listen(CONFIG.port, () => {
     console.log(`Server running at http://localhost:${CONFIG.port}`);
