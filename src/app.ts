e795933229665fe72e501737f4e9dd942b1b2253
gauravPanditr <gauravpandit7856@gmail.com>
2026-01-13 16:11:21 +0530

    fixing index.ts file

--- src/app.ts
+++ src/app.ts
@@ -8,10 +8,10 @@ const app : Express = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
-export const handler = serverless(app);
+
 app.use('/api', apiRouter);
 app.get('/ping', ( res: Response) => {
     res.status(200).json({msg: 'ok with changes'});
 }); 
-
+export const handler = serverless(app);
 export default app;
