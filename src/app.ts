bcca536a568c0e9c662265143876ff95b65199a4
gauravPanditr <gauravpandit7856@gmail.com>
2026-01-13 11:14:58 +0530

    adding vercel json file

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
 import express , { Express, Response } from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-
+import serverless  from "serverless-http"
 import apiRouter from './routes';
 const app : Express = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
-
+export const handler = serverless(app);
 app.use('/api', apiRouter);
-
 app.get('/ping', ( res: Response) => {
     res.status(200).json({msg: 'ok with changes'});
 }); 
