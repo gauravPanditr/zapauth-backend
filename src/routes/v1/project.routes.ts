d4770da2a356e0d50dee3678c9411239438d14ce
gauravPanditr <gauravpandit7856@gmail.com>
2026-01-14 20:50:10 +0530

    fixing router issues

--- src/routes/v1/project.routes.ts
+++ src/routes/v1/project.routes.ts
@@ -3,10 +3,10 @@ import projectController from "../../controller/projectController";
 
 const projectRouter = Router();
 
-// Create project
+
 projectRouter.post("/create", projectController.createProject);
 projectRouter.put("/update",projectController.updateProject);
 projectRouter.delete('/:id',projectController.deleteProjectById);
-
+projectRouter.get('/:id/projects',projectController.getAllProjectsByAdmin);
 
 export default projectRouter;
