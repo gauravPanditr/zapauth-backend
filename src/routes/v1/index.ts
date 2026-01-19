72075ce9c33d5bb8c509633feaa99e42cc4ff03a
gauravPanditr <gauravpandit7856@gmail.com>
2026-01-19 16:34:10 +0530

    removing auth user router

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import adminrouter from './admin.routes';
 import projectRouter from './project.routes';
 import userRouter from './user.routes'
-import authRouter from './auth.routes'
+
 import { authenticateAdmin } from '../../middleware/admin.auth';
 import { validateProject } from '../../middleware/validate-project';
 const v1Router = express.Router();
@@ -10,5 +10,5 @@ const v1Router = express.Router();
 v1Router.use('/admin', adminrouter);
 v1Router.use('/project',authenticateAdmin,projectRouter);
 v1Router.use('/user',validateProject,userRouter);
-v1Router.use('/user',authRouter)
+
 export default v1Router;
