0494aaa8967a69d343b124416c601091f1b84c03
gauravPanditr <gauravpandit7856@gmail.com>
2026-04-13 18:19:47 +0530

    fix the cors

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,21 @@
-import express , { Express, Response } from 'express';
-import bodyParser from 'body-parser';
+import express, { Express, Response } from 'express';
 import cookieParser from 'cookie-parser';
-import serverless  from "serverless-http"
+import serverless from "serverless-http";
 import apiRouter from './routes';
 import cors from "cors";
-const app : Express = express();
-app.use(
-  cors({
-    credentials: true,
-    origin:true,
-  })
-);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
+const app: Express = express();
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors({ credentials: true, origin: true }));
 
 app.use('/api', apiRouter);
-app.get("/ping", ( res: Response) => {
+
+app.get("/ping", (_req, res: Response) => {
   res.status(200).json({ msg: "ok with changes" });
 });
+
 export const handler = serverless(app);
 export default app;
